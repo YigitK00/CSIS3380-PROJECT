23f499e95e2cae395894b2dc102f9ef5b6460adc
YigitK00 <yigitklc123@gmail.com>
2023-07-13 01:10:06 -0700

    Added Menu component to the header

--- src/Header.js
+++ src/Header.js
@@ -1,34 +1,38 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
+import Menu from './Menu';
 
 const Header = () => (
   <header>
-    <nav className="nav-head">
-      <h1>Company Name</h1>
-      <ul className="nav-links">
-        <li>
-          <NavLink to="/">Home</NavLink>
-        </li>
-        <li>
-          <NavLink to="/about">About</NavLink>
-        </li>
-        <li>
-          <NavLink to="/contact">Contact Us</NavLink>
-        </li>
-        <li>
-          <NavLink to="/support">Support</NavLink>
-        </li>
-      </ul>
-      <ul className="login-signup">
-        <li>
-          <NavLink to="/login">Login</NavLink>
-        </li>
-        <li>
-          <NavLink to="/sign-up">Sign up</NavLink>
-        </li>
-      </ul>
-    </nav>
-    <hr />
+    <div>
+      <nav className="nav-head">
+        <h1>Company Name</h1>
+        <ul className="nav-links">
+          <li>
+            <NavLink to="/">Home</NavLink>
+          </li>
+          <li>
+            <NavLink to="/about">About</NavLink>
+          </li>
+          <li>
+            <NavLink to="/contact">Contact Us</NavLink>
+          </li>
+          <li>
+            <NavLink to="/support">Support</NavLink>
+          </li>
+        </ul>
+        <ul className="login-signup">
+          <li>
+            <NavLink to="/login">Login</NavLink>
+          </li>
+          <li>
+            <NavLink to="/sign-up">Sign up</NavLink>
+          </li>
+        </ul>
+      </nav>
+      <hr />
+      <Menu />
+    </div>
   </header>
 );
 
