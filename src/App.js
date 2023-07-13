d118dc12bf287ae9c93626c45e8a2c2e131b3826
YigitK00 <yigitklc123@gmail.com>
2023-07-13 01:09:17 -0700

    Removed the Menu route

--- src/App.js
+++ src/App.js
@@ -6,14 +6,12 @@ import Home from './Home';
 import About from './About';
 import Contact from './Contact';
 import Support from './Support';
-import Menu from './Menu';
 
 const App = () => (
   <div className="wrapper">
     <Header />
     <Routes>
       <Route path="/" element={<Home />} />
-      <Route path="/menu" element={<Menu />} />
       <Route path="/about" element={<About />} />
       <Route path="/contact" element={<Contact />} />
       <Route path="/support" element={<Support />} />
