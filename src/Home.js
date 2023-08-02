5779e833ad1fcad340951dad6d5510ca2574f2ae
Bill Nguyen <BILLNT0711@GMAIL.COM>
2023-08-02 14:47:29 -0700

    Added Cookie extraction function
    Can now extract email address of user who is logged in from cookies to do loan cruds and other front end elements

--- src/Home.js
+++ src/Home.js
@@ -1,8 +1,22 @@
 import React from 'react';
 
+const userEmail = () => {
+  if (document.cookie) {
+    const value = `${document.cookie}`;
+    const regex = /%22(.*)%22/g; // The actual regex
+    const matches = regex.exec(value);
+    const text =  matches[1];
+    const textArray = text.split("%22:%22");
+
+    return ", " + textArray[1] + "!";
+  } else {
+    return "!";
+  }
+}
+
 const Home = () => (
   <div >
-    <p>Welcome Back!</p>
+    <p>Welcome Back{userEmail()}</p>
   </div>
 );
 
