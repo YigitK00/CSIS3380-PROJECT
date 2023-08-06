ca749106a1a10fe52a53653752bffd5a6017adaf
YigitK00 <yigitklc123@gmail.com>
2023-08-06 00:40:06 -0700

    Added the footer

---
+++ src/Frontend/Footer.js
@@ -0,0 +1,23 @@
+const Footer = () => {
+  return (
+    <div>
+      <hr className="footer-hr" />
+      <footer>
+        <p>©2023 Loanwolf, All rights reserved.</p>
+        <ul>
+          <li>
+            <a href="">Privacy Policy</a>
+          </li>
+          <li>
+            <a href="">Terms of Use</a>
+          </li>
+          <li>
+            <a href="">Manage Cookies</a>
+          </li>
+        </ul>
+      </footer>
+    </div>
+  );
+};
+
+export default Footer;
