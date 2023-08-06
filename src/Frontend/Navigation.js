49b13d9978c2eec1a5a47e91a933e1688c5766bb
YigitK00 <yigitklc123@gmail.com>
2023-08-06 04:27:49 -0700

    Made navigation to slide open

--- src/Frontend/Navigation.js
+++ src/Frontend/Navigation.js
@@ -1,29 +1,57 @@
 import React from 'react';
-import { NavLink } from 'react-router-dom';
+import { NavLink, useSearchParams } from 'react-router-dom';
+import { useState } from 'react';
 
-const Navigation = () => (
-  <ul className="menu-links">
-    <li>
-      <NavLink to="/personal-loans">Personal Loans</NavLink>
-    </li>
-    <li>
-      <NavLink to="/business-loans">Business Loans</NavLink>
-    </li>
-    <li>
-      <NavLink to="/car-loans">Car Loans</NavLink>
-    </li>
-    <li>
-      <NavLink to="/mortgage-loans">Mortgage Loans</NavLink>
-    </li>
-    <li>
-      <NavLink to="/consolidation-loans">Consolidation Loans</NavLink>
-    </li>
-    <br />
-    <hr />
-    <li>
-      <NavLink to="/newloan">+ Create a New Loan</NavLink>
-    </li>
-  </ul>
-);
+const Navigation = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const openNav = () => {
+    setIsOpen(true);
+  };
+
+  const closeNav = () => {
+    setIsOpen(false);
+  };
+
+  return (
+    <div>
+      <div className={`side-nav ${isOpen ? 'open' : ''}`}>
+        <a href="javascript:void(0)" className="close-btn" onClick={closeNav}>
+          {' '}
+          &times;{' '}
+        </a>
+        <ul className="menu-links">
+          <li>
+            <NavLink to="/personal-loans">Personal Loans</NavLink>
+          </li>
+          <li>
+            <NavLink to="/business-loans">Business Loans</NavLink>
+          </li>
+          <li>
+            <NavLink to="/car-loans">Car Loans</NavLink>
+          </li>
+          <li>
+            <NavLink to="/mortgage-loans">Mortgage Loans</NavLink>
+          </li>
+          <li>
+            <NavLink to="/consolidation-loans">Consolidation Loans</NavLink>
+          </li>
+          <br />
+          <hr />
+          <li>
+            <NavLink to="/newloan">+ Create a New Loan</NavLink>
+          </li>
+        </ul>
+      </div>
+
+      <div>
+        <span className="open-btn" onClick={openNav}>
+          {' '}
+          &#9776;
+        </span>
+      </div>
+    </div>
+  );
+};
 
 export default Navigation;
