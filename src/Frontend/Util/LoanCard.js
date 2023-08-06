4ffaa9a827be51d0cad18a8d1ca41d9926c97dbc
Bill Nguyen <BILLNT0711@GMAIL.COM>
2023-08-06 00:11:27 -0700

    Update LoanCard.js

--- src/Frontend/Util/LoanCard.js
+++ src/Frontend/Util/LoanCard.js
@@ -3,7 +3,7 @@ import React from "react";
 
 export default function LoanCard (prop){
 return (
-     <div class="card">
+     <div className="card">
           <h2>{prop.name}</h2>
           <span>${prop.amount} interest rate of {prop.interest_rate}% due in {prop.due_in} 
           months will cost ${prop.life_time_cost} over the course of its lifetime.</span>
