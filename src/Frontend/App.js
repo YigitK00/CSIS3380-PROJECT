93a7f360578f04417dc6aa9f364ab8021fc69f4d
YigitK00 <yigitklc123@gmail.com>
2023-08-06 00:41:23 -0700

    called the footer inside App

--- src/Frontend/App.js
+++ src/Frontend/App.js
@@ -5,6 +5,7 @@ import { AuthProvider } from 'react-auth-kit';
 import { useIsAuthenticated } from 'react-auth-kit';
 import { Navigate } from 'react-router-dom';
 import Header from './Header';
+import Footer from './Footer';
 import Home from './Home';
 import About from './navigation/About';
 import Contact from './navigation/Contact';
@@ -21,44 +22,68 @@ import ConsolidationLoans from './loans/ConsolidationLoans';
 
 import NewLoan from './loans/NewLoan';
 
-
 const PrivateRoute = ({ Component }) => {
   const isAuthenticated = useIsAuthenticated();
   const auth = isAuthenticated();
   return auth ? <Component /> : <Navigate to="/login" />;
 };
 
-const LoggedIn = ({Component}) => {
+const LoggedIn = ({ Component }) => {
   const isAuthenticated = useIsAuthenticated();
   const auth = isAuthenticated();
-  return auth ? <Navigate to ="/" />: <Component/> ;
-}
+  return auth ? <Navigate to="/" /> : <Component />;
+};
 
 const App = () => (
   <AuthProvider
-      authType={"cookie"}
-      authName={"_auth"}
-      cookieDomain={window.location.hostname}
-      cookieSecure={false}
+    authType={'cookie'}
+    authName={'_auth'}
+    cookieDomain={window.location.hostname}
+    cookieSecure={false}
   >
     <BrowserRouter>
       <div className="wrapper">
         <Header />
         <Routes>
-          <Route path="/" element={<Home/>} />
+          <Route path="/" element={<Home />} />
           <Route path="/about" element={<About />} />
           <Route path="/contact" element={<Contact />} />
           <Route path="/support" element={<Support />} />
-          <Route path="/login" element={< LoggedIn Component={LoginForm} />} /> 
-          <Route path="/register" element={< LoggedIn Component={RegisterForm} />} />
-          <Route path="/personal-loans" element={<PrivateRoute Component={PersonalLoans} />} />
-          <Route path="/car-loans" element={<PrivateRoute Component={CarLoans} />}  />
-          <Route path="/business-loans" element={<PrivateRoute Component={BusinessLoans} />}  />
-          <Route path="/business-loans" element={<PrivateRoute Component={BusinessLoans} />}  />
-          <Route path="/mortgage-loans" element={<PrivateRoute Component={MortgageLoans} />}  />
-          <Route path="/consolidation-loans" element={<PrivateRoute Component={ConsolidationLoans} />}  />
-          <Route path="/newloan" element={<PrivateRoute Component={NewLoan} />}  />
+          <Route path="/login" element={<LoggedIn Component={LoginForm} />} />
+          <Route
+            path="/register"
+            element={<LoggedIn Component={RegisterForm} />}
+          />
+          <Route
+            path="/personal-loans"
+            element={<PrivateRoute Component={PersonalLoans} />}
+          />
+          <Route
+            path="/car-loans"
+            element={<PrivateRoute Component={CarLoans} />}
+          />
+          <Route
+            path="/business-loans"
+            element={<PrivateRoute Component={BusinessLoans} />}
+          />
+          <Route
+            path="/business-loans"
+            element={<PrivateRoute Component={BusinessLoans} />}
+          />
+          <Route
+            path="/mortgage-loans"
+            element={<PrivateRoute Component={MortgageLoans} />}
+          />
+          <Route
+            path="/consolidation-loans"
+            element={<PrivateRoute Component={ConsolidationLoans} />}
+          />
+          <Route
+            path="/newloan"
+            element={<PrivateRoute Component={NewLoan} />}
+          />
         </Routes>
+        <Footer />
       </div>
     </BrowserRouter>
   </AuthProvider>
